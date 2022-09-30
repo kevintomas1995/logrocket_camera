c30092466a6555eba026649725f10aa5dd66de17
Kevin Tomas <kevintomas@MacBook-Pro-2.local>
2022-09-30 11:05:25 +0200

    moved userPermission Hook

--- App.js
+++ App.js
@@ -1,77 +1,77 @@
 import { Camera } from "expo-camera";
 import { Image, StyleSheet, Text, View } from "react-native";
-import * as MediaLibrary from "expo-media-library";
+// import * as MediaLibrary from "expo-media-library";
 import { useState, useRef, useEffect } from "react";
-import Button from "./src/components/Button";
-import Timer from "./src/components/Timer";
-import usePermission from "./src/components/usePermisson";
+// import Button from "./src/components/Button";
+// import Timer from "./src/components/Timer";
+import usePermission from "./src/usePermisson";
 
 export default function App() {
   const hasCameraPermissions = usePermission(Camera);
-  const [image, setImage] = useState(null);
+  // const [image, setImage] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.front);
-  const [timerClicked, setTimerClicked] = useState(false);
-  const [timer, setTimer] = useState(0);
-  const [displayTimer, setDisplayTimer] = useState(timer);
-  const [timerOn, setTimerOn] = useState(false);
-  const camreaRef = useRef(null);
+  // const [timerClicked, setTimerClicked] = useState(false);
+  // const [timer, setTimer] = useState(0);
+  // const [displayTimer, setDisplayTimer] = useState(timer);
+  // const [timerOn, setTimerOn] = useState(false);
+  const cameraRef = useRef(null);
 
-  const takePicture = async () => {
-    setTimerOn(true);
-    setTimeout(async function () {
-      if (camreaRef) {
-        try {
-          const data = await camreaRef.current.takePictureAsync();
-          setImage(data.uri);
-          setTimerOn(false);
-        } catch (error) {
-          console.log(error);
-        }
-      }
-    }, timer * 1000);
-  };
+  // const takePicture = async () => {
+  //   setTimerOn(true);
+  //   setTimeout(async function () {
+  //     if (camreaRef) {
+  //       try {
+  //         const data = await camreaRef.current.takePictureAsync();
+  //         setImage(data.uri);
+  //         setTimerOn(false);
+  //       } catch (error) {
+  //         console.log(error);
+  //       }
+  //     }
+  //   }, timer * 1000);
+  // };
 
-  useEffect(() => {
-    if (!timerOn) {
-      return;
-    }
-    setDisplayTimer(timer);
+  // useEffect(() => {
+  //   if (!timerOn) {
+  //     return;
+  //   }
+  //   setDisplayTimer(timer);
 
-    const interval = setInterval(() => {
-      setDisplayTimer((prevTimer) => prevTimer - 1);
-    }, 1000);
+  //   const interval = setInterval(() => {
+  //     setDisplayTimer((prevTimer) => prevTimer - 1);
+  //   }, 1000);
 
-    return () => {
-      setDisplayTimer(timer);
-      clearInterval(interval);
-    };
-  }, [timerOn, setTimerOn, timer]);
+  //   return () => {
+  //     setDisplayTimer(timer);
+  //     clearInterval(interval);
+  //   };
+  // }, [timerOn, setTimerOn, timer]);
 
-  useEffect(() => {
-    if (displayTimer === 0) {
-      return () => {
-        setDisplayTimer(timer);
-        setTimerOn(false);
-      };
-    }
-  }, [timer]);
+  // useEffect(() => {
+  //   if (displayTimer === 0) {
+  //     return () => {
+  //       setDisplayTimer(timer);
+  //       setTimerOn(false);
+  //     };
+  //   }
+  // }, [timer]);
 
-  const onPressTimerItem = (time) => {
-    setTimerClicked((prevState) => !prevState);
-    setTimer(time);
-  };
+  // const onPressTimerItem = (time) => {
+  //   setTimerClicked((prevState) => !prevState);
+  //   setTimer(time);
+  // };
 
-  const savePicture = async () => {
-    if (image) {
-      try {
-        const asset = await MediaLibrary.createAssetAsync(image);
-        alert("Image saved to camera roll");
-        setImage(null);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  };
+  // const savePicture = async () => {
+  //   if (image) {
+  //     try {
+  //       const asset = await MediaLibrary.createAssetAsync(image);
+  //       alert("Image saved to camera roll");
+  //       setImage(null);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   }
+  // };
 
   if (hasCameraPermissions === false) {
     return <Text>No permission to access camera</Text>;
@@ -79,10 +79,10 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      {timerClicked && <Timer onPress={onPressTimerItem} />}
-      {!image ? (
-        <Camera style={styles.camera} type={type} ref={camreaRef}>
-          <View style={styles.buttonContainer}>
+      {/* {timerClicked && <Timer onPress={onPressTimerItem} />} */}
+      {/* {!image ? ( */}
+        <Camera style={styles.camera} type={type} ref={cameraRef}>
+          {/* <View style={styles.buttonContainer}>
             <Button
               icon={"retweet"}
               title="Flip"
@@ -103,15 +103,15 @@ export default function App() {
               />
               <Text style={styles.timerText}>{timer}s</Text>
             </View>
-          </View>
-          {timerOn && (
+          </View> */}
+          {/* {timerOn && (
             <Text style={styles.displayTimerText}>{displayTimer}s</Text>
-          )}
+          )} */}
         </Camera>
-      ) : (
-        <Image style={styles.camera} source={{ uri: image }} />
-      )}
-      <View>
+      {/* ) : ( */}
+        {/* <Image style={styles.camera} source={{ uri: image }} /> */}
+      {/* )} */}
+      {/* <View>
         {image ? (
           <View style={styles.takenImage}>
             <Button
@@ -128,7 +128,7 @@ export default function App() {
             onPress={takePicture}
           />
         )}
-      </View>
+      </View> */}
     </View>
   );
 }
